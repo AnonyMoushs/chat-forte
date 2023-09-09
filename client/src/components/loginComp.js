abf9d6093f68c95dcd031ff005edcb3405426a55
Fkad23 <144503036+AnonyMoushs@users.noreply.github.com>
2023-09-09 13:23:57 +0800

    Update loginComp.js

--- client/src/components/loginComp.js
+++ client/src/components/loginComp.js
@@ -22,7 +22,7 @@ const LoginComponents = () => {
   const Submit = (e) => {
     e.preventDefault()
 
-    Axios.post("https://apps-akowp4kqp-anonymoushs.vercel.app/loginUser", loginInfo).then((response) => {
+    Axios.post("https://chat-forte-eoa9so6vp-anonymoushs.vercel.app/loginUser", loginInfo).then((response) => {
 
     console.log(response.data.message)
 
